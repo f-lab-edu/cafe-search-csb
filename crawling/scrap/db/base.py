2c5732890aefb85b1fa2c5b55de12b9795d7aef3
oortcloud96 <oortcloud96@gmail.com>
2022-10-19 16:15:22 +0900

    add sqlalchemy setup

---
+++ crawling/scrap/db/base.py
@@ -0,0 +1,19 @@
+from typing import Any
+from sqlalchemy import Column, Integer, DateTime, func
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+
+class BaseMixin:
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, nullable=False, default=func.utc_timestamp())
+    updated_at = Column(DateTime, nullable=False, default=func.utc_timestamp(), onupdate=func.utc_timestamp())
---
+++ crawling/scrap/db/session.py
@@ -0,0 +1,24 @@
+from typing import Generator
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from config import settings
+
+engine = create_engine(
+    "mysql+pymysql://{username}:{password}@{host}:{port}/{name}?charset=utf8mb4".format(
+        username=settings.DB_USERNAME,
+        password=settings.DB_PASSWORD.get_secret_value(),
+        host=settings.DB_HOST,
+        port=settings.DB_PORT,
+        name=settings.DB_NAME,
+    )
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
