72bded89f64bb9ffc5a59fd06ca8a71275804595
oortcloud96 <oortcloud96@gmail.com>
2022-10-19 16:12:06 +0900

    add config.py for get db information and remote host ip

---
+++ crawling/scrap/config.py
@@ -0,0 +1,50 @@
+from pydantic import BaseSettings, SecretStr
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "KaKao Crawl"
+    PROJECT_VERSION: str = "1.0.0"
+
+    DB_USERNAME: str
+    DB_PASSWORD: SecretStr
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+
+    CRAWL_HOST1: str
+    CRAWL_HOST2: str
+    CRAWL_HOST3: str
+    CAFE_FN: str
+
+    class Config:
+        env_file = str(BASE_DIR / ".env")
+        env_file_encoding = "utf-8"
+
+
+class TestSettings(BaseSettings):
+    DB_USERNAME: str
+    DB_PASSWORD: SecretStr
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME = "test"
+
+    CRAWL_HOST1: str
+    CRAWL_HOST2: str
+    CRAWL_HOST3: str
+
+    class Config:
+        env_file = str(BASE_DIR / ".env")
+        env_file_encoding = "utf-8"
+
+
+def get_settings():
+    if os.getenv("APP_ENV", "DEVELOP") == "TEST":
+        return TestSettings()
+    return Settings()
+
+
+settings = get_settings()
