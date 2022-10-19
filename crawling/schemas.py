c90c8faeaaf6f29335c7219c2fa5b726bb2cb8df
oortcloud96 <oortcloud96@gmail.com>
2022-10-19 16:08:18 +0900

    remove unnecessary files

--- crawling/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class Cafe(BaseModel):
-    address: Optional[str]
-    phone: Optional[str]
-    tags: list
-    img_url: Optional[str]
-    facilities: list
