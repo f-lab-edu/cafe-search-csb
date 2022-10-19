bf21630e7a811a1f388ebf90b462f20f19f21f11
oortcloud96 <oortcloud96@gmail.com>
2022-10-19 16:17:43 +0900

    change cafe db schema - add N:M realtionship with facilities and location full name

--- backend/db/models/cafes.py
+++ backend/db/models/cafes.py
@@ -1,14 +1,28 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Table, Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 from db.base_class import Base
 from db.models.base import BaseMixin
 
 
+cafe_facility = Table(
+    "cafe_facility",
+    Base.metadata,
+    Column("cafe_id", ForeignKey("cafe.id")),
+    Column("facility_id", ForeignKey("facility.id"))
+)
+
+
 class Cafe(Base, BaseMixin):
     cafename = Column(String(20), nullable=False)
-    location = Column(String(50), nullable=False)
+    phone=Column(String(20))
+    jibeonfullname = Column(String(150), nullable=False)
+    dorofullname = Column(String(150), nullable=False)
+    imageurl = Column(String(100))
+    tags = Column(String(100))
     comments = relationship("Comment", back_populates="cafe")
+    able_facilities = relationship("Facility", secondary=cafe_facility, back_populates="cafes")
+    disable_facilities = relationship("Facility", secondary=cafe_facility, back_populates="cafes")
 
 
 class Comment(Base, BaseMixin):
@@ -18,3 +32,8 @@ class Comment(Base, BaseMixin):
     userid = Column(Integer, ForeignKey("user.id"))
     user = relationship("User", back_populates="comments")
     cafe = relationship("Cafe", back_populates="comments")
+
+
+class Facility(Base, BaseMixin):
+    name = Column(String(10), nullable=False)
+    cafes = relationship("Cafe", secondary=cafe_facility, back_populates="facilities")
