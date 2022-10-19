17b504d97ab710637a9f775a9b13d302769816a6
oortcloud96 <oortcloud96@gmail.com>
2022-10-19 16:05:38 +0900

    add main.py - send request to remote host and get response about cafe information

--- crawling/main.py
+++ crawling/main.py
@@ -1,38 +1,98 @@
+import asyncio
+import csv
+import time
+from typing import Tuple, Union, List
 import aiohttp
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from schemas import Cafe
+from scrap.config import settings
+from scrap.utils import RESPONSE_TYPE
 
-app = FastAPI()
 
-async def scrap_cafe(id: int):
-    url = f"https://place.map.kakao.com/main/v/{id}"
+url_map = {
+    settings.CRAWL_HOST1: 0,
+    settings.CRAWL_HOST2: 0,
+    settings.CRAWL_HOST3: 0
+}
+
+INTERVAL = 10
+FAILURE_INTERVAL = 60
+
+
+async def make_queue(filename: str) -> asyncio.Queue:
+    """
+    output:
+    queue: [["스타벅스", 1234], ["탐앤탐스", 5678]]
+    """
+    queue = asyncio.Queue()
+    with open(filename, "r", encoding="utf-8") as fr:
+        rdr = csv.DictReader(fr, fieldnames=['name', 'id'])
+        for line in rdr:
+            id, name = line['id'], line['name']
+            queue.put_nowait((name, id))
+    return queue
+
+
+def print_result(results: List[RESPONSE_TYPE], queue: asyncio.Queue) -> None:
+    """
+    resp_json["status"]에 따라 다른 결과 출력
+    status
+      success : 크롤링 성공
+      Fail : KAKAO MAP 크롤링 실패. scrap_server 사이드 문제
+      Connect-Failed : CRAWL_HOST 연결 실패. main의 ClientSession 문제
+    """
+    for result in results:
+        url, name, id, resp_json = result
+        if resp_json["status"] == "Fail":
+            url_map[url] = FAILURE_INTERVAL
+            queue.put_nowait((name, id))
+            print(f"{url} BANNED")
+        elif resp_json["status"] == "success":
+            print(f"{url} - {name} saved")
+        elif resp_json["status"] == "Connect-Failed":
+            print(f"Session Closed")
+            queue.put_nowait((name, id))
+
+
+async def fetch(
+    session: aiohttp.ClientSession,
+    url: str,
+    name: str,
+    id: int
+) -> Tuple[str, str, int, RESPONSE_TYPE]:
+    try:
+        async with session.get(f"{url}/scrap/{id}") as response:
+            resp_json = await response.json()
+    except:
+        resp_json = {"status": "connection-failed"}
+    finally:
+        return (url, name, id, resp_json)
+
+
+async def send_request(queue: asyncio.Queue, session: aiohttp.ClientSession):
+    """
+    failure_interval
+      - 특정 CRAWL_HOST가 KAKAO MAP 연결 실패 시 60으로 설정
+      - 한번의 루프(10초) 마다 failure_interval 1씩 감소. 총 600초(10분) 동안 해당 호스트 중지
+    """
+    fetchers = []
+    for url, failure_interval in url_map.items():
+        if failure_interval == 0:
+            try:
+                name, id = await queue.get()
+                fetchers.append(asyncio.create_task(fetch(session, url, name, id)))
+            except asyncio.QueueEmpty:
+                break
+        else:
+            failure_interval -= 1
+    results = await asyncio.gather(*fetchers)
+    print_result(results, queue)
+
+
+async def main():
+    queue = await make_queue(settings.CAFE_FN)
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            page = await response.json()
-            if not page["isExist"]:
-                return
-    
-    basic_info = page["basicInfo"]
-    address = basic_info.get("address")
-    phone = basic_info.get("phonenum")
-    tags = basic_info.get("tags")
-    img_url = basic_info.get("mainphotourl")
-    facilities = basic_info.get("facilityInfo")
-    
-    cafe = Cafe(
-        address=address,
-        phone=phone,
-        tags=tags,
-        img_url=img_url,
-        facilities=facilities
-    )
+        while queue:
+            await send_request(queue, session)
+            await asyncio.sleep(INTERVAL)
 
-    return cafe
-
-@app.get("/scrap/{id}")
-async def get_cafe(id: int):
-    cafe = await scrap_cafe(id)
-    cafe = jsonable_encoder(cafe)
-    return JSONResponse(content=cafe)
+if __name__ == "__main__":
+    asyncio.run(main())
