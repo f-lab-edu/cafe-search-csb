2c06be65bd04027a09d8f12c21da961f3b49af9f
oortcloud96 <oortcloud96@gmail.com>
2022-10-23 20:37:33 +0900

    add test data and change test utility

--- backend/tests/test_routes/test_cafes.py
+++ backend/tests/test_routes/test_cafes.py
@@ -9,20 +9,15 @@ from tests.conftest import other_user_token_headers
 
 def test_read_cafe(client, admin_token_headers):
     create_cafe(client, admin_token_headers)
-    response = client.get("/cafes/get/1")
+    response = client.get("/cafes/1")
 
     assert response.status_code == 200
     assert response.json()["cafename"] == "cafe1"
 
 
 def test_read_all_cafes(client, admin_token_headers):
-    datas = [
-        {"cafename": "cafe1", "location": "testlocation1"},
-        {"cafename": "cafe2", "location": "testlocation2"},
-    ]
-    for data in datas:
-        client.post("/cafes/create", data=json.dumps(data), headers=admin_token_headers)
-    response = client.get("cafes/all")
+    create_cafe(client, admin_token_headers)
+    response = client.get("cafes")
     print(response.json())
 
     assert response.status_code == 200
@@ -56,13 +51,13 @@ def test_delete_cafe_fail_by_user(
     client, normal_user_token_headers, admin_token_headers
 ):
     create_cafe(client, admin_token_headers)
-    response = client.delete("/cafes/delete/1", headers=normal_user_token_headers)
+    response = client.delete("/cafes/1", headers=normal_user_token_headers)
     assert response.status_code == 401
 
 
 def test_delete_cafe_success_by_admin(client, admin_token_headers):
     create_cafe(client, admin_token_headers)
-    response = client.delete("/cafes/delete/1", headers=admin_token_headers)
+    response = client.delete("/cafes/1", headers=admin_token_headers)
     assert response.status_code == 200
     assert response.json()["msg"] == "Deleted"
 
@@ -70,7 +65,7 @@ def test_delete_cafe_success_by_admin(client, admin_token_headers):
 def test_read_comments(client, normal_user_token_headers, admin_token_headers):
     create_cafe(client, admin_token_headers)
     create_comment(client, normal_user_token_headers)
-    response = client.get("cafes/1/comments", headers=normal_user_token_headers)
+    response = client.get("cafes/1/comment", headers=normal_user_token_headers)
 
     assert response.status_code == 200
     assert response.json()[0]["comment"] == "Good!"
@@ -82,7 +77,7 @@ def test_delete_comment_success_by_same_user(
     create_cafe(client, admin_token_headers)
     create_comment(client, normal_user_token_headers)
     response = client.delete(
-        "cafes/comments/1/delete", headers=normal_user_token_headers
+        "cafes/comment/1", headers=normal_user_token_headers
     )
     assert response.status_code == 200
     assert response.json()["msg"] == "Deleted"
@@ -94,6 +89,6 @@ def test_delete_comment_fail_by_other_user(
     create_cafe(client, admin_token_headers)
     create_comment(client, normal_user_token_headers)
     response = client.delete(
-        "cafes/comments/1/delete", headers=other_user_token_headers
+        "cafes/comments/1", headers=other_user_token_headers
     )
     assert response.status_code == 401
--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -30,8 +30,30 @@ def authentication_token_from_email(
 
 
 def create_cafe(client: TestClient, headers: Dict[str, str]):
-    data = {"cafename": "cafe1", "location": "testlocation"}
-    client.post("/cafes/create", data=json.dumps(data), headers=headers)
+    datas = [
+        {
+            "cafename" : "cafe1",
+            "phone" : "02-222-2222",
+            "jibeonfullname" : "서울 동대문구 회기동 124-1",
+            "dorofullname" : "서울 동대문구 이문로 125",
+            "imageurl" : "",
+            "tags" : "분위기좋은 데이트코스",
+            "able_facilities" : ["흡연", "wifi"],
+            "disable_facilities" : ["주차"]
+        },
+        {
+            "cafename" : "cafe2",
+            "phone" : "02-333-3333",
+            "jibeonfullname" : "서울 마포구 망원동 425-33",
+            "dorofullname" : "서울 마포구 망원로 7길 31-14",
+            "imageurl" : "www.www.www.",
+            "tags" : "로스팅카페",
+            "able_facilities" : ["주차", "wifi"],
+            "disable_facilities" : ["놀이방", "흡연"]
+        }
+    ]
+    for data in datas:
+        client.post("/cafes/create", data=json.dumps(data), headers=headers)
 
 
 def create_comment(client: TestClient, headers: Dict[str, str]):
