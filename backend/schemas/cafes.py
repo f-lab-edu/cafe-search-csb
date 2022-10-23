22fc84b3bd3a6c9e708b2384a0decdcfa2ae408c
oortcloud96 <oortcloud96@gmail.com>
2022-10-23 20:38:05 +0900

    add pydantic schemas for cafe

--- backend/schemas/cafes.py
+++ backend/schemas/cafes.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple
+from enum import Enum
+from typing import List, Optional
 from pydantic import BaseModel
 
 class CommentCreate(BaseModel):
@@ -13,19 +14,62 @@ class ShowComment(BaseModel):
     class Config:
         orm_mode = True
 
+
+class ShowFacility(BaseModel):
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class ShowTag(BaseModel):
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class Facility(str, Enum):
+    smokingromm = "흡연"
+    fordisabled = "장애인시설"
+    nuersery = "놀이방"
+    parking = "주차"
+    wifi = "wifi"
+    pet = "애완동물출입"
+
+
 class CafeCreate(BaseModel):
     cafename: str
-    location: str
+    phone: Optional[str]
+    jibeonfullname: str
+    dorofullname: str
+    imageurl: Optional[str]
+    tags: Optional[str]
+    able_facilities: List[Facility]
+    disable_facilities: List[Facility]
+
+
+class CafeUpdate(BaseModel):
+    cafename: Optional[str]
+    phone: Optional[str]
+    jibeonfullname: Optional[str]
+    dorofullname: Optional[str]
+    imageurl: Optional[str]
+    tags: Optional[str]
+    able_facilities: Optional[List[Facility]]
+    disable_facilities: Optional[List[Facility]]
 
 
 class ShowCafe(BaseModel):
     cafename: str
-    location: str
+    phone: str
+    jibeonfullname: str
+    dorofullname: str
     comments: List[ShowComment]
+    able_facilities: List[ShowFacility]
+    disable_facilities: List[ShowFacility]
+    tags: List[ShowTag]
 
     class Config:
         orm_mode = True
         arbitrary_types_allowed = True
-
-
-
